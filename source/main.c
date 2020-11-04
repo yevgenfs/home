1a6216f27801186a3ce220298ab60958a7592ed3
yevgenfs <44088277+yevgenfs@users.noreply.github.com>
2020-11-04 15:03:54 +0200

    queue test

--- source/main.c
+++ source/main.c
@@ -1,11 +1,138 @@
 // main.c 
 // folder home
 #include <stdio.h>
-int main(int argc, char const *argv[])
+
+typedef struct  
+{ 
+	// Initialize front and rear 
+	int rear, front; 
+
+	// Circular Queue 
+	int size; 
+	int *arr; 
+
+
+}Queue; 
+
+Queue queue;
+
+void create(int s, Queue *q)
 {
-    printf("hello world \n");
-    getchar();
-
-
-    return 0;
+	q->front = -1; 
+    q->rear = -1;
+	q->size = s; 
+    int array[s]; 
+	q->arr = &array;
 }
+
+/* Function to create Circular queue */
+void enQueue( Queue *q, int value) 
+{ 
+	if ((q->front == 0 && q->rear == q->size-1) || 
+			(q->rear == (q->front-1)%(q->size-1))) 
+	{ 
+		printf("\nQueue is Full"); 
+		return; 
+	} 
+
+	else if (q->front == -1) /* Insert First Element */
+	{ 
+		q->front = 0;
+        q->rear = 0; 
+		*(q->arr + q->rear) = value; 
+	} 
+
+	else if (q->rear == q->size-1 && q->front != 0) 
+	{ 
+		q->rear = 0; 
+		*(q->arr + q->rear) = value; 
+	} 
+
+	else
+	{ 
+		q->rear++; 
+		*(q->arr + q->rear) = value; 
+	} 
+} 
+
+// Function to delete element from Circular Queue 
+int deQueue(Queue *q) 
+{ 
+	if (q->front == -1) 
+	{ 
+		printf("\nQueue is Empty"); 
+		return NULL; 
+	} 
+
+	int data = *(q->arr + q->front); 
+	*(q->arr + q->front) = -1; 
+	if (q->front == q->rear) 
+	{ 
+		q->front = -1; 
+		q->rear = -1; 
+	} 
+	else if (q->front == q->size-1) 
+		q->front = 0; 
+	else
+		q->front++; 
+
+	return data; 
+} 
+
+// Function displaying the elements 
+// of Circular Queue 
+void displayQueue(Queue *q) 
+{ 
+	if (q->front == -1) 
+	{ 
+		printf("\nQueue is Empty"); 
+		return; 
+	} 
+	printf("\nElements in Circular Queue are: "); 
+	if (q->rear >= q->front) 
+	{ 
+		for (int i = q->front; i <= q->rear; i++) 
+			printf("%d ",*(q->arr + i)); 
+	} 
+	else
+	{ 
+		for (int i = q->front; i < q->size; i++) 
+			printf("%d ", *(q->arr + i)); 
+
+		for (int i = 0; i <= q->rear; i++) 
+			printf("%d ", *(q->arr + i)); 
+	} 
+} 
+
+/* Driver of the program */
+int main() 
+{ 
+	Queue q; 
+	Queue *pointer = &q;
+     
+	create(5, pointer);
+	// Inserting elements in Circular Queue 
+	enQueue(14, pointer); 
+	enQueue(22, pointer); 
+	enQueue(13, pointer); 
+	enQueue(-6, pointer); 
+
+	// Display elements present in Circular Queue 
+	displayQueue(pointer); 
+
+	// Deleting elements from Circular Queue 
+	printf("\nDeleted value = %d", deQueue(pointer)); 
+	printf("\nDeleted value = %d", deQueue(pointer)); 
+
+	displayQueue(pointer); 
+
+	enQueue(9, pointer); 
+	enQueue(20, pointer); 
+	enQueue(5, pointer); 
+
+	displayQueue(pointer); 
+
+	enQueue(20, pointer); 
+	return 0; 
+} 
+
