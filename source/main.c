1176ee3cc47d65c5efce81d503d02b040898e880
yevgenfs <44088277+yevgenfs@users.noreply.github.com>
2020-11-06 13:21:04 +0200

    remove extra pointer and corect malloc

--- source/main.c
+++ source/main.c
@@ -17,9 +17,7 @@ void create( int s, Queue* q)
 	q->front = -1;
 	q->rear = -1;
 	q->size = s;
-	//maloc
-	int array[s];
-	if(((q->arr =(int*)malloc(sizeof(array)))) == NULL)
+	if(((q->arr =(int*)malloc(s * sizeof(int)))) == NULL)
 	{
 		printf("Allocation error.");
 		exit(0);
@@ -113,58 +111,55 @@ void delete (Queue *q)
 /* Driver of the program */
 int main()
 {
-	Queue q;
-	Queue* pointer = &q;
-
+	Queue first_q;
 	Queue second_q;
-	Queue* second_pointer = &second_q;
 	//create
-	create(10, pointer);
-	create(10, second_pointer);
+	create(10, &first_q);
+	create(10, &second_q);
 	// Inserting elements in Circular Queue 
-	enQueue(14, pointer);
-	enQueue(22, pointer);
-	enQueue(13, pointer);
-	enQueue(-6, pointer);
+	enQueue(14, &first_q);
+	enQueue(22, &first_q);
+	enQueue(13, &first_q);
+	enQueue(-6, &first_q);
 
-	enQueue(1, second_pointer);
-	enQueue(2, second_pointer);
-	enQueue(3, second_pointer);
-	enQueue(-4, second_pointer);
+	enQueue(1, &second_q);
+	enQueue(2, &second_q);
+	enQueue(3, &second_q);
+	enQueue(-4, &second_q);
  
 	// Display elements present in Circular Queue 
-	displayQueue(pointer);
-	displayQueue(second_pointer);
+	displayQueue(&first_q);
+	displayQueue(&second_q);
  
 	// Deleting elements from Circular Queue 
-	printf("\nDeleted value = %d", deQueue(pointer));
-	printf("\nDeleted value = %d", deQueue(pointer));
-	printf("\nDeleted value = %d", deQueue(second_pointer));
-	printf("\nDeleted value = %d", deQueue(second_pointer));
+	printf("\nDeleted value = %d", deQueue(&first_q));
+	printf("\nDeleted value = %d", deQueue(&first_q));
+	printf("\nDeleted value = %d", deQueue(&second_q));
+	printf("\nDeleted value = %d", deQueue(&second_q));
 
     // Display elements present in Circular Queue 
-	displayQueue(pointer);
-	displayQueue(second_pointer);
+	displayQueue(&first_q);
+	displayQueue(&second_q);
 
 	// Inserting elements in Circular Queue 
-	enQueue(9, pointer);
-	enQueue(20, pointer);
-	enQueue(5, pointer);
+	enQueue(9, &first_q);
+	enQueue(20, &first_q);
+	enQueue(5, &first_q);
 
-	enQueue(11, second_pointer);
-	enQueue(12, second_pointer);
-	enQueue(13, second_pointer);
+	enQueue(11, &second_q);
+	enQueue(12, &second_q);
+	enQueue(13, &second_q);
  
 	// Display elements present in Circular Queue 
-	displayQueue(pointer);
-	displayQueue(second_pointer);
+	displayQueue(&first_q);
+	displayQueue(&second_q);
 
 	// test for full queue
-	enQueue(14, second_pointer);
-	enQueue(15, second_pointer);
-	enQueue(16, second_pointer);
-	enQueue(17, second_pointer);
-	enQueue(18, second_pointer);
-	enQueue(19, second_pointer);
+	enQueue(14, &second_q);
+	enQueue(15, &second_q);
+	enQueue(16, &second_q);
+	enQueue(17, &second_q);
+	enQueue(18, &second_q);
+	enQueue(19, &second_q);
 	return 0;
 }
